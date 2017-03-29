ae526aaace7d88d8109fec9dfe9111552209169d
Max? <mgrinchenko@forcemktg.com>
2017-03-28 21:59:56 -0400

    Header file was not included in last commit; however it is a rather crucial component of the site. Web.config file in case future user wants to run this on iis

---
+++ pieces/header.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mgrinchenko
+ * Date: 3/27/2017
+ * Time: 6:56 PM
+ */
+
+?>
+
+
+<head>
+<!--meta-->
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="A tool for comparing airplane ticket prices across multiple destinations and dates">
+
+<title> Flight Finder </title>
+<!--font-->
+    
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<!-- All cdn based component may be found within public/css/lib and public/js/lib
+<!--css-->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="/public/css/main.css">
+    
+<!--js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.7/angular.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/2.5.0/ui-bootstrap-tpls.min.js"></script>
+<script src="/public/js/lib/chart.min.js"></script>
+<script src="/public/js/lib/angular-chart.js"></script>
+<script src="/public/js/app.js"></script>
+
+</head>
